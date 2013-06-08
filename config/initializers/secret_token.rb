c4bd36e77ba601743d757aaf3b3f31f7a3b5dcd9
whowantstolivefo <whowantstolivefo@gmail.com>
2013-06-08 09:53:48 +0300

    static3 first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Static3::Application.config.secret_key_base = '3ab10739b7adff79b2e14ffd40050392b01d1121439ce4d1aaeffc97746c82132ddbbd68c7c8c9dce9a2fdeba8b73624ed4b7d16efe305a3b9fdd251488b9079'
